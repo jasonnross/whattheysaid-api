d9af5595411b2676144137d78494013149334a39
jasonnross <jasonnross@gmail.com>
2022-05-24 15:24:38 -0700

    start

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors')
 const app = express();
-const port = 8081;
+const port = 8080;
 const articlesRouter = require("./routes/articlesRouter");
 const personsRouter = require("./routes/personsRouter");
 
